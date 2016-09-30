f226d5e0a7cfc1312b1d8c60e49c2a1e57da3302
Imran Khan <imran.khan@adi-mps.com>
2016-09-30 11:35:41 +0530

    [v2]:Entry of ServletContextListener is removed. ApplicationContextAware interface is used with more convenient way.

--- src/main/java/mypack/aware/SetUp.java
+++ src/main/java/mypack/aware/SetUp.java
@@ -7,6 +7,16 @@ import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
+/**
+ * @author i.khan
+ * Object of this class is loaded(as we have bean entry for this file in ApplicationContext) when the application 
+ * is deployed, because we have entry in web.xml file for ContextLoaderListener. 
+ * This lister loads ApplicationContext at the time of deployment and all the beans are loaded when 
+ * applicationContext.xml is read.
+ * Note: When object of ApplicationContext is created it creates all the objects for all beans present in the 
+ * applicationContext.xml. 
+ *
+ */
 public class SetUp implements ApplicationContextAware {
 
 	@Override
