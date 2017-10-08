5bb31999f10152c422171ef6f8f51367b0f7afd8
Imran Khan <imran.khan@adi-mps.com>
2017-10-08 10:20:21 +0530

    [2.0-SNAPSHOT]:Upgraded spring version

--- src/main/java/mypack/IndexController.java
+++ src/main/java/mypack/IndexController.java
@@ -2,13 +2,20 @@ package mypack;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.ParameterizableViewController;
+
+import com.mypack.service.TestService;
 public class IndexController extends ParameterizableViewController
 {
 
+	@Autowired
+	private TestService service;//It is not working
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    	
+    	//service.test();
         System.out.println("###############    IndexController class ################333");
         ModelAndView v=new ModelAndView();
         v.addObject("name","This is test application");
